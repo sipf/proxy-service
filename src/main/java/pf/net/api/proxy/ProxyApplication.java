dd85d7b29dd1dc28eea759b30fd1a0f7b9f556c5
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2017-09-19 22:49:48 -1000

    It's better when Zuul is enabled

--- src/main/java/pf/net/api/proxy/ProxyApplication.java
+++ src/main/java/pf/net/api/proxy/ProxyApplication.java
@@ -2,7 +2,9 @@ package pf.net.api.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ProxyApplication {
 
